e5d7671de2da0835c54105cdb2f86cbcf01a63e8
mcdermottm97 <98967026+mcdermottm97@users.noreply.github.com>
2022-02-11 14:55:54 +0000

    First draft for login page
    very basic, non-functional draft of login page with title, text input, and a button

--- App.js
+++ App.js
@@ -1,20 +1,89 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { Component } from 'react';
+import { StyleSheet, Text, View, TextInput, Button, FlatList } from 'react-native'
 
-export default function App() {
+
+const Flex = () => {
   return (
-    <View style={styles.container}>
-      <Text>Hello world!</Text>
-      <StatusBar style="auto" />
+    <View style={styles.container1}>
+
+      <View style={styles.container1}>
+        <View style={styles.box1}>
+          <Text style={styles.title}>
+            Welcome to Spacebook
+          </Text>
+        </View>
+      </View>
+
+      <View style={styles.container2}>
+        <View style={styles.box1} >
+          <Text>
+            Login here
+          </Text>
+          <TextInput
+            style={styles.input}
+            placeholder="username"
+          />
+          <TextInput
+            style={styles.input}
+            placeholder="password"
+          />
+          <Button
+            title='LOG IN'
+          />
+        </View>
+      </View>
+
     </View>
+
+    
   );
-}
+};
 
 const styles = StyleSheet.create({
-  container: {
+  container1: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    padding: 20,
+    flexDirection: "column",
+    backgroundColor: "black"
   },
+  container2: {
+    flex: 4,
+    padding: 20,
+    flexDirection: "row"
+  },
+  box1: {
+    flex: 1,
+    padding: 5,
+    backgroundColor: "white",
+    justifyContent: 'center',
+    alignItems: 'center'
+    
+  },
+  box2: {
+    flex: 1,
+    padding: 5,
+    backgroundColor: "darkorange" 
+  },
+  box3: {
+    flex: 1,
+    padding: 5,
+    backgroundColor: "green" 
+  },
+  box4: {
+    flex: 1,
+    backgroundColor: "blue" 
+  },
+  input: {
+    backgroundColor: 'white',
+    borderColor: "gray",
+    borderWidth: 1,
+    borderRadius: 10,
+    padding: 10,
+  },
+  title: {
+    fontSize: 30
+  }
+
 });
+
+export default Flex;
