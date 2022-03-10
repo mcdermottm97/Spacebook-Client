0d4d14c676d9f9a17d159e4963647cd78360e097
mcdermottm97 <98967026+mcdermottm97@users.noreply.github.com>
2022-03-10 16:35:08 +0000

    Update login.js

--- components/login.js
+++ components/login.js
@@ -1,33 +1,73 @@
 import React, { Component } from 'react';
-import {
-  Text, View, Button, TextInput,
-} from 'react-native';
+import { Button } from 'react-native';
+import { ScrollView, TextInput } from 'react-native-gesture-handler';
+import AsyncStorage from '@react-native-async-storage/async-storage';
 
 class LoginScreen extends Component {
   constructor(props) {
     super(props);
-    this.state = {};
+
+    this.state = {
+      email: '',
+      password: '',
+    };
   }
 
-  render() {
-    const { navigation } = this.props;
+  login = async () => {
+    return fetch('http://localhost:3333/api/1.0.0/login', {
+      method: 'post',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify(this.state),
+    })
+      .then((response) => {
+        if (response.status === 200) {
+          return response.json();
+        } if (response.status === 400) {
+          throw 'Invalid email or password';
+        } else {
+          throw 'Something went wrong';
+        }
+      })
+      .then(async (responseJson) => {
+        console.log(responseJson);
+        await AsyncStorage.setItem('@session_token', responseJson.token);
+        this.props.navigation.navigate('home');
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  };
 
+  render() {
     return (
-      <View>
-        <Text>Welcome to spacebook</Text>
-        <TextInput placeholder="email" />
-        <TextInput placeholder="password" />
-        <Button
-          title="log in"
-          onPress={() => navigation.navigate('Home')}
+      <ScrollView>
+        <TextInput
+          placeholder="Enter your email..."
+          onChangeText={(email) => this.setState({ email })}
+          value={this.state.email}
+          style={{ padding: 5, borderWidth: 1, margin: 5 }}
+        />
+        <TextInput
+          placeholder="Enter your password..."
+          onChangeText={(password) => this.setState({ password })}
+          value={this.state.password}
+          secureTextEntry
+          style={{ padding: 5, borderWidth: 1, margin: 5 }}
         />
         <Button
-          title="don't have an account?  sign up here!"
-          onPress={() => navigation.navigate('Sign Up')}
+          title="Login"
+          onPress={() => this.login()}
         />
-      </View>
+        <Button
+          title="Don't have an account?"
+          onPress={() => this.props.navigation.navigate('signup')}
+        />
+      </ScrollView>
     );
   }
 }
 
 export default LoginScreen;
+
