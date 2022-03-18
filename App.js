72f8090d4180b1af08ea730f35213c58e96dde12
mcdermottm97 <98967026+mcdermottm97@users.noreply.github.com>
2022-03-18 17:31:01 +0000

    Update App.js
    updated with new comments

--- App.js
+++ App.js
@@ -1,15 +1,20 @@
-/* eslint-disable react/prefer-stateless-function */
-/* eslint-disable import/extensions */
-/* eslint-disable import/no-unresolved */
-import 'react-native-gesture-handler';
-import * as React from 'react';
-import { Component } from 'react';
-import { NavigationContainer } from '@react-navigation/native';
-import { createNativeStackNavigator } from '@react-navigation/native-stack'; // lint found problem resolving path to module, disabled for this file
+/*
+Matthew McDermott - 18/03/2022
 
-import HomeScreen from './components/home';
+For this project I used ESLint to enforce AirBnB style.
+In some areas I was unable to follow the style, for
+each case I have disabled the rule in the '.eslintrc.js'
+file.
+*/
+
+import 'react-native-gesture-handler';
+import React, { Component } from 'react';
+import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+
 import LoginScreen from './components/login';
 import SignupScreen from './components/signup';
+import AccountNavigation from './components/account';
 
 const Stack = createNativeStackNavigator();
 
@@ -17,10 +22,14 @@ class App extends Component {
   render() {
     return (
       <NavigationContainer>
-        <Stack.Navigator initialRouteName="login">
-          <Stack.Screen name="home" component={HomeScreen} />
+        <Stack.Navigator>
           <Stack.Screen name="login" component={LoginScreen} />
           <Stack.Screen name="signup" component={SignupScreen} />
+          <Stack.Screen
+            name="account"
+            component={AccountNavigation}
+            options={{ headerShown: false }}
+          />
         </Stack.Navigator>
       </NavigationContainer>
     );
@@ -28,4 +37,3 @@ class App extends Component {
 }
 
 export default App;
-
